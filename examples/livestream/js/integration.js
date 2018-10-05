d79122428420677150d21b33e940248d8eac0df0
averytt <averywhere18@gmail.com>
2018-10-04 18:42:22 -0700

    Updated Theta (HLS.js) example

--- examples/livestream/js/integration.js
+++ examples/livestream/js/integration.js
@@ -1,16 +1,16 @@
 'use strict';
 
+const PEER_SERVER_HOST = "prod-theta-peerjs.thetatoken.org";
+const PEER_SERVER_PORT = 8700;
+const TRACKER_SERVER_HOST = "prod-testnet-grouping.thetatoken.org";
+const TRACKER_SERVER_PORT = 8700;
 
-const VIDEO_URL = "https://video.yourplatform.io/playlist.m3u8" 
-const PLATFORM_THETA_WALLET_SERVICE_URL = "https://api.yourplatform.io/wallet/theta/";
-const PEER_SERVER_URL      = "https://api.yourtracker.io";
-const PEER_SERVER_PORT     = 9000;
-const TRACKER_SERVER_URL   = "https://api.yourtracker.io";
-const TRACKER_SERVER_PORT  = 3000;
-const VIDEO_ID  = 'vid123';
+// TODO Fill these in with your data
+const PLATFORM_THETA_WALLET_SERVICE_URL = "https://wallet.myplatform.com/v1/theta/";
+const VIDEO_ID = 'vid123';
 const LOGGED_IN_USER_ID = 'usr123';
 const LOGGED_IN_USER_AUTH_TOKEN = 'tok123';
-
+const VIDEO_URL = "https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8";
 
 // --------- Platform Theta Wallet ------------
 
@@ -19,54 +19,63 @@ class PlatformThetaWalletService extends Theta.BaseWallet {
         let userId = LOGGED_IN_USER_ID;
         let authToken = LOGGED_IN_USER_AUTH_TOKEN;
 
-        if(userId && authToken){
+        if (userId && authToken) {
             return {
                 userId: userId,
-                authToken : authToken
+                authToken: authToken
             }
         }
-        else{
+        else {
             // No user is logged in, don't call the server.
             return null;
         }
     }
 
-    async _makeRPC(name, args) {
+    async
+
+    _makeRPC(name, args) {
         let data = {
             "jsonrpc": "2.0",
             "method": name,
             "params": [args],
             "id": "1"
         };
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) = > {
             let auth = this.getAuth();
 
-            if(auth){
-                // Configure the API call to your server adding headers, auth, etc.
+        if (auth) {
+            // Configure the API call to your server adding headers, auth, etc.
 
-                $.ajax({
-                    url: this.config.endpoint,
-                    data: JSON.stringify(data),
-                    type: 'POST',
-                    headers: {
-                        'X-Auth-User': auth.userId,
-                        'X-Auth-Token': auth.authToken,
-                        'Content-Type': 'application/json'
-                    }
-                }).done((resp) => {
-                    if (resp.error) {
-                        reject(resp.error);
-                    } else {
-                        resolve(resp.result);
-                    }
-                }).fail((resp) => {
-                    reject(resp);
-                });
+            $.ajax({
+                url: this.config.endpoint,
+                data: JSON.stringify(data),
+                type: 'POST',
+                headers: {
+                    'X-Auth-User': auth.userId,
+                    'X-Auth-Token': auth.authToken,
+                    'Content-Type': 'application/json'
+                }
+            }).done((resp) = > {
+                if(resp.error
+        )
+            {
+                reject(resp.error);
             }
-            else{
-                // No user us logged in.
+        else
+            {
+                resolve(resp.result);
             }
-        });
+        }).
+            fail((resp) = > {
+                reject(resp);
+        })
+            ;
+        }
+        else {
+            // No user us logged in.
+        }
+    })
+        ;
     }
 }
 
@@ -75,12 +84,26 @@ class PlatformThetaWalletService extends Theta.BaseWallet {
 
 function startVideo(theta) {
     class ClosuredThetaLoader extends Theta.HlsJsFragmentLoader {
-        load(...args) {
-            // Inject context from closure.
-            this.thetaCtx = theta;
-            super.load(...args);
-        }
+        load(
+
+    ...
+        args
+    ) {
+        // Inject context from closure.
+        this
+    .
+        thetaCtx = theta;
+        super
+    .
+
+        load(
+
+    ...
+        args
+    )
+        ;
     }
+}
 
     let hlsOpts = (theta ? {fLoader: ClosuredThetaLoader} : {});
     let videoURL = VIDEO_URL;
@@ -100,7 +123,7 @@ function startVideo(theta) {
         // We are not using HLS.js, so Theta will not be able to use P2P!
 
         videoElement.src = videoURL;
-        videoElement.addEventListener('loadedmetadata',function() {
+        videoElement.addEventListener('loadedmetadata', function () {
             videoElement.play();
         });
     }
@@ -113,7 +136,7 @@ function startVideo(theta) {
             hls.loadSource(videoURL);
         });
 
-        hls.on(Hls.Events.MANIFEST_PARSED,function(event,data) {
+        hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
             // Start playback
             videoElement.play();
         });
@@ -127,73 +150,72 @@ function startPlayer() {
     let userId = LOGGED_IN_USER_ID;
 
     let wallet = new PlatformThetaWalletService({
-            endpoint: PLATFORM_THETA_WALLET_SERVICE_URL,
-        });
-        wallet.start();
+        endpoint: PLATFORM_THETA_WALLET_SERVICE_URL
+    });
+    wallet.start();
 
-        let theta = new Theta({
-            fragmentSize: 5000,
-            failoverFactor: 0.7,
-            fragmentTimeout: 200,
-            statsReportInterval: 90000,
-            peerReqInterval: 120000,
+    let theta = new Theta({
+        fragmentSize: 5000,
+        failoverFactor: 0.7,
+        fragmentTimeout: 3000,
+        probeTimeout: 600,
+        statsReportInterval: 90000,
+        peerReqInterval: 120000,
 
-            videoId: VIDEO_ID,
-            userId: userId,
-            wallet: wallet,
+        videoId: VIDEO_ID,
+        userId: userId,
+        wallet: wallet,
 
-            peerServer: {
-                host: PEER_SERVER_URL,
-                port: PEER_SERVER_PORT,
-                secure: true
-            },
-            trackerServer: {
-                host: TRACKER_SERVER_URL,
-                port: TRACKER_SERVER_PORT,
-                secure: true,
-                path: ""
-            },
+        peerServer: {
+            host: PEER_SERVER_HOST,
+            port: PEER_SERVER_PORT,
+            secure: true
+        },
+        trackerServer: {
+            host: TRACKER_SERVER_HOST,
+            port: TRACKER_SERVER_PORT,
+            secure: true,
+            path: ""
+        },
 
-            debug: false
-        });
+        debug: true
+    });
 
-        // Event handlers
-        theta.addEventListener(Theta.Events.PEERS_CHANGED, function (data) {
-            // Connected peers changed
-            // Data:
-            // totalPeers : Integer
-        });
-        theta.addEventListener(Theta.Events.TRAFFIC, function (data) {
-            // Bandwidth was used
-            // Data:
-            // type : String ('cdn', 'p2p_inbound', 'p2p_outbound')
-            // stats : Object
-            // stats.size : Integer - Total bytes
-        });
-        theta.addEventListener(Theta.Events.PAYMENT_RECEIVED, function (data) {
-            // Payment received
-            // Data:
-            // payment : Object - info about the payment
-            // payment.amount : Integer - Payment amount in GammaWei
-        });
-        theta.addEventListener(Theta.Events.PAYMENT_SENT, function (data) {
-            // Payment sent
-            // Data:
-            // payment : Object - info about the payment
-            // payment.amount : Integer - Payment amount in GammaWei
-        });
-        theta.addEventListener(Theta.Events.ACCOUNT_UPDATED, function (data) {
-            // Account/waller updated
-            // Data:
-            // account : Object - info about the account/wallet
-        });
-        theta.start();
+    // Event handlers
+    theta.addEventListener(Theta.Events.PEERS_CHANGED, function (data) {
+        // Connected peers changed
+        // Data:
+        // totalPeers : Integer
+    });
+    theta.addEventListener(Theta.Events.TRAFFIC, function (data) {
+        // Bandwidth was used
+        // Data:
+        // type : String ('cdn', 'p2p_inbound', 'p2p_outbound')
+        // stats : Object
+        // stats.size : Integer - Total bytes
+    });
+    theta.addEventListener(Theta.Events.PAYMENT_RECEIVED, function (data) {
+        // Payment received
+        // Data:
+        // payment : Object - info about the payment
+        // payment.amount : Integer - Payment amount in GammaWei
+    });
+    theta.addEventListener(Theta.Events.PAYMENT_SENT, function (data) {
+        // Payment sent
+        // Data:
+        // payment : Object - info about the payment
+        // payment.amount : Integer - Payment amount in GammaWei
+    });
+    theta.addEventListener(Theta.Events.ACCOUNT_UPDATED, function (data) {
+        // Account/waller updated
+        // Data:
+        // account : Object - info about the account/wallet
+    });
+    theta.start();
 
-        startVideo(theta);
+    startVideo(theta);
 }
 
 function startApp() {
     startPlayer();
 }
-
-startApp();
